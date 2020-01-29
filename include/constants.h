acaf870a91e1333c8ef14756c711cb0aeae1f11b
Ben Hermans <ben.hermans2@student.kuleuven.be>
2020-01-29 17:20:45 +0100

    Basic constants

---
+++ include/constants.h
@@ -0,0 +1,12 @@
+#define TRUE 1
+#define FALSE 0
+
+#define SUCCESS 1
+#define FAIL 0
+
+#define NONE 0
+
+#define UNSYMMETRIC 0
+#define UPPER 1 /**< Use only upper part of matrix*/
+#define LOWER 2 /**< Use only lower part of matrix*/
+
