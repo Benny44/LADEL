530287d53b592aa863abae009335703423e0d5cf
Ben Hermans <ben.hermans2@student.kuleuven.be>
2020-01-31 10:50:20 +0100

    Basic build setup (cmake)

---
+++ shell/buildTest.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#Assume this is called in the LADEL directory
+curdir=`pwd`
+
+#Build direcetories
+if [ ! -d "build" ]; then
+  mkdir build
+fi
+
+if [ ! -d "build/debug" ]; then
+  mkdir build/debug
+fi
+
+if [ ! -d "build/lib" ]; then
+  mkdir build/lib
+fi
+
+builddir=$curdir/build/debug
+cd $builddir
+
+cmake $curdir -DCMAKE_BUILD_TYPE=debug -DUNITTESTS=ON 
+make
+ctest -VV
