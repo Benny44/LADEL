b1e0a106ee1c45187f0fae5d1015304893d05bd9
Ben Hermans <ben.hermans2@student.kuleuven.be>
2020-01-31 14:13:16 +0100

    Matvec

--- src/matvec.c
+++ src/matvec.c
@@ -4,10 +4,22 @@
 
 void ladel_matvec(const ladel_sparse_matrix *M, const ladel_double *x, ladel_double *y, ladel_int reset)
 {
-
+    ladel_int index, col, row;
+    if (reset)
+    {
+        for (index = 0; index < M->nrow; index++) y[index] = 0;
+    }
+    for (col = 0; col < M->ncol; col++)
+    {
+        for (index = M->p[col]; index < M->p[col+1]; index++)
+        {
+            row = M->i[index];
+            y[row] += M->x[index] * x[col];
+        }
+    }
 }
 
 void ladel_tpose_matvec(const ladel_sparse_matrix *M, const ladel_double *x, ladel_double *y, ladel_int reset)
 {
-
+    
 }
