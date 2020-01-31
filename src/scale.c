aaae432143318000630463c7341b5eda586485af
Ben Hermans <ben.hermans2@student.kuleuven.be>
2020-01-31 13:44:20 +0100

    Infinity norm per row

--- src/scale.c
+++ src/scale.c
@@ -49,5 +49,11 @@ void ladel_infinity_norm_columns(ladel_sparse_matrix *M, ladel_double *norms)
 
 void ladel_infinity_norm_rows(ladel_sparse_matrix *M, ladel_double *norms)
 {
-
+    ladel_int index, row;
+    for (row = 0; row < M->nrow; row++) norms[row] = 0;
+    for (index = 0; index < M->nzmax; index++)
+    {
+        row = M->i[index];
+        norms[row] = LADEL_MAX(norms[row], LADEL_ABS(M->x[index]));
+    }
 }
