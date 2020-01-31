f146cb9c06b498519c043f9684986a7babf20ad3
Ben Hermans <ben.hermans2@student.kuleuven.be>
2020-01-31 14:08:12 +0100

    Skeleton and tests for matvec

---
+++ src/matvec.c
@@ -0,0 +1,13 @@
+#include "global.h"
+#include "types.h"
+#include "matvec.h"
+
+void ladel_matvec(const ladel_sparse_matrix *M, const ladel_double *x, ladel_double *y, ladel_int reset)
+{
+
+}
+
+void ladel_tpose_matvec(const ladel_sparse_matrix *M, const ladel_double *x, ladel_double *y, ladel_int reset)
+{
+
+}
--- test/src/run_all_tests.c
+++ test/src/run_all_tests.c
@@ -1,9 +1,11 @@
 #include "minunit.h"
 #include "test_scale.h"
+#include "test_matvec.h"
 
 int main(){
     MU_INITIALIZE();
     MU_RUN_SUITE(suite_scale);
+    MU_RUN_SUITE(suite_matvec);
     MU_REPORT();
     return minunit_fail;
 }
