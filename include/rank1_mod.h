ab80f130d5c44682c456e4b7af0ad02cc79936aa
Ben Hermans <ben.hermans2@student.kuleuven.be>
2020-02-26 10:01:30 +0100

    Introduce col struct

--- include/rank1_mod.h
+++ include/rank1_mod.h
@@ -9,11 +9,22 @@ typedef struct ladel_set_struct {
     ladel_int max_size_set;
 } ladel_set;
 
+typedef struct ladel_col_struct {
+    ladel_int *i;
+    ladel_double *x;
+    ladel_int nz;
+    ladel_int nzmax;
+} ladel_col;
+
 ladel_set *ladel_init_set(ladel_int *set_vals, ladel_int size_set, ladel_int max_size_set);
 
-ladel_int ladel_add_nonzero_pattern_to_L(ladel_int *Li, ladel_double *Lx, ladel_int *nz_L, ladel_int nzmax_L, ladel_int *set, ladel_int size_set);
+ladel_col *ladel_init_col(ladel_int *i, ladel_double *x, ladel_int nz , ladel_int nzmax);
 
-ladel_int ladel_set_union(ladel_set *first_set, ladel_set *second_set);
+void ladel_set_col(ladel_col* col,ladel_int *i, ladel_double *x, ladel_int nz , ladel_int nzmax);
+
+ladel_int ladel_add_nonzero_pattern_to_L(ladel_col *col, ladel_set *set);
+
+ladel_int ladel_set_union(ladel_set *first_set, ladel_set *second_set, ladel_set *difference, ladel_int *offset);
 
 ladel_int ladel_rank1_update(ladel_factor **LD, ladel_symbolics **sym, ladel_sparse_matrix *W, ladel_int col_in_W);
 
