ac24132a84b8c7fa8a9e47bddf52145c1fe09016
Ben Hermans <ben.hermans2@student.kuleuven.be>
2020-05-06 13:26:10 +0200

    Add some timings in the example

--- interfaces/mex/ladel_test_permutation.m
+++ interfaces/mex/ladel_test_permutation.m
@@ -4,22 +4,27 @@ if exist('solver')
     clear solver
 end
 
-test_cholmod = false;
+test_cholmod = true;
 
 ordering = 1; %1 for AMD, 0 for natural ordering
 
+tic
 n = 1000;
 M = sprand(n,n, 1e-1, 1) + 2*speye(n);
 M = (M+M')/2;
 x = rand(n,1);
+times.generate.M = toc;
 
 %% Example 1: factorize and solve
 solver = ladel(n);
-[L,D,p] = solver.factorize(M, ordering);
+% [L,D,p] = solver.factorize(M, ordering);
+solver.factorize(M, ordering);
+
 y = solver.dense_solve(x);
 assert(norm(y-M\x) < 1e-12);
 
 %% Example 2: factorize_advanced and row_mod
+tic;
 Mbasis = sprand(n,n, 2e-1, 1) + 3*speye(n);
 Mbasis = (Mbasis+Mbasis')/2;
 
@@ -30,18 +35,24 @@ d = rand(1);
 M(n/2,n/2) = 1; 
 
 Mbasis = Mbasis + M; %make sure entries of M are in Mbasis
+times.generate.Mbasis = toc;
 
 % clear all
 % load('problematic');
+% clear times
 % clear solver
 % solver = ladel(n);
 % ordering = 1;
-
-[L,D,p] = solver.factorize_advanced(M, Mbasis, ordering);
+tic;
+% [L,D,p] = solver.factorize_advanced(M, Mbasis, ordering);
+solver.factorize_advanced(M, Mbasis, ordering);
+times.ladel.factorize = toc;
 if test_cholmod
+    tic;
     [LD,~, LD_p] = ldlchol(M);
     LD_pinv = 1:n;
     LD_pinv(LD_p) = LD_pinv;
+    times.chol.factorize = toc;
 end
 
 y = solver.dense_solve(x);
@@ -54,11 +65,17 @@ end
 %% ADD row using row_mod
 clear row;
 row = Mbasis(:,n/2);
-[Lupd,Dupd,p] = solver.row_mod(n/2, row, full(Mbasis(n/2,n/2)));
+tic;
+% [Lupd,Dupd,p] = solver.row_mod(n/2, row, full(Mbasis(n/2,n/2)));
+solver.row_mod(n/2, row, full(Mbasis(n/2,n/2)));
+times.ladel.rowadd = toc;
 
 if test_cholmod
     row = Mbasis(:,n/2);
-    LD = ldlrowmod(LD, LD_pinv(n/2), row(LD_p));
+    tic;
+    row = row(LD_p);
+    LD = ldlrowmod(LD, LD_pinv(n/2), row);
+    times.chol.rowadd = toc;
 end
 
 Mupd = M;
@@ -77,13 +94,22 @@ assert(norm(y-Mupd\x) < 1e-12);
 % We delete the row we just added again
 
 if test_cholmod
+    tic;
     LD = ldlrowmod(LD, LD_pinv(n/2));
+    times.chol.rowdel = toc;
     y_chol = ldl_perm_solve(LD, LD_p, x);
     assert(norm(y_chol-M\x) < 1e-12);
 end
 
-[Lfinal, Dfinal, p] = solver.row_mod(n/2);
+tic;
+% [Lfinal, Dfinal, p] = solver.row_mod(n/2);
+solver.row_mod(n/2);
+times.ladel.rowdel = toc;
+
 y = solver.dense_solve(x);
 assert(norm(y-M\x) < 1e-12);
 
 solver.delete();
+
+times.ladel
+times.chol
