21596af7eeb7df9b2d2d011876a2c97398d52716
Ben Hermans <ben.hermans2@student.kuleuven.be>
2020-05-19 11:45:58 +0200

    Add test where nz is used in a factorization

--- test/src/test_ldl.c
+++ test/src/test_ldl.c
@@ -17,7 +17,7 @@
 static ladel_work *work;
 static ladel_work *work2;
 static ladel_sparse_matrix *M, *Mbasis;
-static ladel_sparse_matrix *M2;
+static ladel_sparse_matrix *M2, *Mnz;
 static ladel_symbolics *sym;
 static ladel_factor *LD;
 static ladel_symbolics *sym2;
@@ -40,6 +40,14 @@ void ldl_suite_setup(void)
     M2->p[0] = 0; M2->p[1] = 1; M2->p[2] = 2; M2->p[3] = 3; M2->p[4] = 5; 
     M2->i[0] = 0; M2->i[1] = 1; M2->i[2] = 2; M2->i[3] = 0; M2->i[4] = 3; 
     M2->x[0] = 2; M2->x[1] = 1; M2->x[2] = 3; M2->x[3] = 1; M2->x[4] = 2; 
+
+    /* Adding one value in the col_pointers (from col 3 onwards) */
+    Mnz = ladel_sparse_alloc(NROW, NCOL, NZMAX+1, UPPER, TRUE, TRUE);
+    Mnz->p[0] = 0; Mnz->p[1] = 1; Mnz->p[2] = 3; Mnz->p[3] = 5; Mnz->p[4] = 8; Mnz->p[5] = 11;
+    Mnz->nz[0] = 1; Mnz->nz[1] = 2; Mnz->nz[2] = 1; Mnz->nz[3] = 3; Mnz->nz[4] = 3; 
+
+    Mnz->i[0] = 0; Mnz->i[1] = 0; Mnz->i[2] = 1; Mnz->i[3] = 2; Mnz->i[4] = 52342304; Mnz->i[5] = 1; Mnz->i[6] = 2; Mnz->i[7] = 3; Mnz->i[8] = 0; Mnz->i[9] = 3; Mnz->i[10] = 4;  
+    Mnz->x[0] = 1; Mnz->x[1] = 10; Mnz->x[2] = 2; Mnz->x[3] = -3; Mnz->x[4] = 454359; Mnz->x[5] = 11; Mnz->x[6] = 12; Mnz->x[7] = 4; Mnz->x[8] = -3; Mnz->x[9] = 2; Mnz->x[10] = -5;
 }
 
 void ldl_suite_teardown(void)
@@ -49,6 +57,7 @@ void ldl_suite_teardown(void)
     ladel_sparse_free(M);
     ladel_sparse_free(Mbasis);
     ladel_sparse_free(M2);
+    ladel_sparse_free(Mnz);
 }
 
 void ldl_test_setup(void)
@@ -92,6 +101,23 @@ MU_TEST(test_simple_ldl2)
     ladel_int index;
 
     for (index = 0; index < NCOL2; index++)
+        mu_assert_double_eq(y[index], y_ref[index], TOL);
+}
+
+MU_TEST(test_simple_ldl_nz)
+{
+    ladel_double x[NCOL] = {1, 2, 3, 4, 5};
+    ladel_double y[NCOL], y_ref[NCOL] = {-1.738103756708408e-01, -1.081932021466905e-01, 4.379964221824687e-01, 3.594991055456172e-01, -7.519141323792485e-01};
+
+    ladel_print_sparse_matrix_matlab(Mnz);
+
+    ladel_int status = ladel_factorize(Mnz, sym, NO_ORDERING, &LD, work);
+    
+    mu_assert_long_eq(status, SUCCESS);
+    ladel_dense_solve(LD, x, y, work);
+    ladel_int index;
+
+    for (index = 0; index < NCOL; index++)
         mu_assert_double_eq(y[index], y_ref[index], TOL);
 }
 
@@ -175,6 +201,7 @@ MU_TEST_SUITE(suite_ldl)
     MU_SUITE_CONFIGURE(ldl_suite_setup, ldl_suite_teardown, ldl_test_setup, ldl_test_teardown);
     MU_RUN_TEST(test_simple_ldl);
     MU_RUN_TEST(test_simple_ldl2);
+    MU_RUN_TEST(test_simple_ldl_nz);
     MU_RUN_TEST(test_simple_ldl_with_diag);
     MU_RUN_TEST(test_simple_ldl_with_partial_diag);
     #ifdef DAMD
