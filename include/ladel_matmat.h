ec547889a4fd66f78f01212356edc320f57580d1
Ben Hermans <ben.hermans2@student.kuleuven.be>
2020-06-18 10:38:08 +0200

    Function declaration added in header

--- include/ladel_matmat.h
+++ include/ladel_matmat.h
@@ -5,6 +5,8 @@
 
 ladel_sparse_matrix *ladel_mat_mat_transpose(ladel_sparse_matrix *M, ladel_sparse_matrix *M_transpose, ladel_work *work);
 
+ladel_sparse_matrix *ladel_mat_mat_transpose_pattern(ladel_sparse_matrix *M, ladel_sparse_matrix *M_transpose, ladel_work *work);
+
 ladel_sparse_matrix *ladel_mat_diag_mat_transpose(ladel_sparse_matrix *M, ladel_sparse_matrix *M_transpose, ladel_double *diag, ladel_work *work);
 
 ladel_sparse_matrix *ladel_mat_mat_transpose_advanced(ladel_sparse_matrix *M, ladel_sparse_matrix *M_transpose, ladel_double *diag, int values, ladel_work *work);
