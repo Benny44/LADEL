97025d4d3e66f0cf5ea20c1bca8f8578fa52b6eb
Ben Hermans <ben.hermans2@student.kuleuven.be>
2020-06-24 16:51:29 +0200

    Updated documentation

--- docs/search/defines_5.js
+++ docs/search/defines_5.js
@@ -1,9 +1,9 @@
 var searchData=
 [
-  ['ladel_5fabs',['LADEL_ABS',['../ladel__global_8h.html#ac9efc8bdaee1cf7f3746cd31d619376e',1,'ladel_global.h']]],
-  ['ladel_5ffor',['LADEL_FOR',['../ladel__global_8h.html#a24c2c233cfd029e141204421cada50c1',1,'ladel_global.h']]],
-  ['ladel_5fmax',['LADEL_MAX',['../ladel__global_8h.html#a35bab7688c40bb1809b9e3a39aaacf0f',1,'ladel_global.h']]],
-  ['ladel_5fmin',['LADEL_MIN',['../ladel__global_8h.html#aaeecd08f2e8c44a14ba11b35174585ea',1,'ladel_global.h']]],
+  ['ladel_5fabs',['LADEL_ABS',['../ladel__constants_8h.html#ac9efc8bdaee1cf7f3746cd31d619376e',1,'ladel_constants.h']]],
+  ['ladel_5ffor',['LADEL_FOR',['../ladel__constants_8h.html#a24c2c233cfd029e141204421cada50c1',1,'ladel_constants.h']]],
+  ['ladel_5fmax',['LADEL_MAX',['../ladel__constants_8h.html#a35bab7688c40bb1809b9e3a39aaacf0f',1,'ladel_constants.h']]],
+  ['ladel_5fmin',['LADEL_MIN',['../ladel__constants_8h.html#aaeecd08f2e8c44a14ba11b35174585ea',1,'ladel_constants.h']]],
   ['ladel_5fprint',['ladel_print',['../ladel__global_8h.html#abb18383af23620ea925a494580f08a89',1,'ladel_global.h']]],
   ['lower',['LOWER',['../ladel__constants_8h.html#aaa9295df3f8f6033cae4b89cc7e5ff29',1,'ladel_constants.h']]]
 ];
