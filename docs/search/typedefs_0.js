1261d9b9c4461c6f0a86c6735da6c5cb78fba203
Ben Hermans <ben.hermans2@student.kuleuven.be>
2020-06-26 12:19:22 +0200

    Updated documentation

--- docs/search/typedefs_0.js
+++ docs/search/typedefs_0.js
@@ -4,7 +4,7 @@ var searchData=
   ['ladel_5fdiag',['ladel_diag',['../ladel__types_8h.html#a2ef43a657b064e9795a2ab191271e6e6',1,'ladel_types.h']]],
   ['ladel_5fdouble',['ladel_double',['../ladel__types_8h.html#ac9052e1e18b442b104c3ee5dcd86557b',1,'ladel_types.h']]],
   ['ladel_5ffactor',['ladel_factor',['../ladel__types_8h.html#a684c52fe0f1b3e4e44f0b99d4e410f6a',1,'ladel_types.h']]],
-  ['ladel_5fint',['ladel_int',['../ladel__types_8h.html#a3d32cb07c3dc394a7b6ae479dd02068e',1,'ladel_types.h']]],
+  ['ladel_5fint',['ladel_int',['../ladel__types_8h.html#a05486496308d021b6b4f64e8def1f451',1,'ladel_types.h']]],
   ['ladel_5fset',['ladel_set',['../ladel__types_8h.html#af0104c40d20f1207bccae48c74fe74f4',1,'ladel_types.h']]],
   ['ladel_5fsparse_5fmatrix',['ladel_sparse_matrix',['../ladel__types_8h.html#a1581a514e7a6bc6f8ba19e265c006dc4',1,'ladel_types.h']]],
   ['ladel_5fsymbolics',['ladel_symbolics',['../ladel__types_8h.html#a3d5c70070f210ea7893b32f9ec39a504',1,'ladel_types.h']]],
