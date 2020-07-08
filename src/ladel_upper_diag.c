bd3d195e4ebed7a21a8dc31e9a9bd2acfcbc7ff2
Ben Hermans <ben.hermans2@student.kuleuven.be>
2020-07-08 20:25:10 +0200

    Fix to upper diag for UPPER matrices (same as default)

--- src/ladel_upper_diag.c
+++ src/ladel_upper_diag.c
@@ -7,38 +7,30 @@
 void ladel_to_upper_diag(ladel_sparse_matrix *M)
 {
     ladel_int index, row, col, Mptemp, nzM = 0;
-    ladel_sparse_matrix *Mt;
 
-    switch (M->symmetry)
+    if (M->symmetry == LOWER)
     {
-    case UPPER:
-        /* do nothing */
-        __attribute__ ((fallthrough));
-    case LOWER:
-        /* first transpose the matrix */
-        Mt = ladel_transpose(M, TRUE, NULL);
+        ladel_sparse_matrix *Mt = ladel_transpose(M, TRUE, NULL);
         ladel_sparse_copy(Mt, M);
         ladel_sparse_free(Mt);
-        __attribute__ ((fallthrough));
-    default:
-        for (col = 0; col < M->ncol; col++)
+    }
+
+    for (col = 0; col < M->ncol; col++)
+    {
+        Mptemp = M->p[col];
+        M->p[col] = nzM;
+        for (index = Mptemp; index < ((M->nz) ? Mptemp + M->nz[col] : M->p[col+1]); index++)
         {
-            Mptemp = M->p[col];
-            M->p[col] = nzM;
-            for (index = Mptemp; index < ((M->nz) ? Mptemp + M->nz[col] : M->p[col+1]); index++)
+            row = M->i[index];
+            if (row <= col)
             {
-                row = M->i[index];
-                if (row <= col)
-                {
-                    M->i[nzM] = row;
-                    if (M->values) M->x[nzM] = M->x[index];
-                    nzM++;
-                }
+                M->i[nzM] = row;
+                if (M->values) M->x[nzM] = M->x[index];
+                nzM++;
             }
         }
-        M->p[M->ncol] = nzM;
-        ladel_sparse_realloc(M, nzM);
-        M->symmetry = UPPER;
-        break;
-    }   
+    }
+    M->p[M->ncol] = nzM;
+    ladel_sparse_realloc(M, nzM);
+    M->symmetry = UPPER;
 }
